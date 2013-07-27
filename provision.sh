1586388d58ec3c2c50d1342cc93991a7da0ad915
Jon Mosco <jonny.mosco@gmail.com>
2013-07-26 21:09:53 -0400

    updated provision.sh to be more verbose

--- provision.sh
+++ provision.sh
@@ -24,16 +24,39 @@ NAME=$( hostname -s )
 case $NAME in 
 	puppet)
 		echo "$HOSTS" >> /etc/hosts
-		/sbin/service iptables stop && chkconfig iptables off
+
+		echo "Turning off Firewall and setting selinux to Permissive on our Puppet Master..."
+		/sbin/service iptables stop > /dev/null && chkconfig iptables off > /dev/null
 		/usr/sbin setenforce 0
-		/bin/rpm -ivh http://yum.puppetlabs.com/el/6/products/i386/puppetlabs-release-6-7.noarch.rpm
-		/usr/bin/yum install puppet-server -y
-		/usr/bin/puppet resource service puppetmaster ensure=running enable=true
+		echo "Done"
+
+		echo "Installing Puppet Master.."
+		/bin/rpm -ivh http://yum.puppetlabs.com/el/6/products/i386/puppetlabs-release-6-7.noarch.rpm \
+			> /dev/null 2>&1
+		/usr/bin/yum install puppet-server -y > /dev/null 2>&1
+		echo "Done"
+
+		echo "Starting Puppet Master Service"
+		/usr/bin/puppet resource service puppetmaster ensure=running enable=true > /dev/null 2>&1
+		master=$?
+		if [ $master -eq 0 ]; then
+			echo "Done"
+		fi
 		;;
 	node)
 		echo "$HOSTS" >> /etc/hosts
-		/sbin/service iptables stop && chkconfig iptables off
+
+		echo "Turning off Firewall and setting selinux to Permissive on our Puppet Node..."
+		/sbin/service iptables stop > /dev/null 2>&1 \
+			&& chkconfig iptables off > /dev/null 2>&1
 		/usr/sbin setenforce 0
-		/usr/bin/puppet resource service puppet ensure=running enable=true
+		echo "Done"
+
+		echo "Starting Puppet Agent Service"
+		/usr/bin/puppet resource service puppet ensure=running enable=true > /dev/null 2>&1
+		agent=$?
+		if [ $agent -eq 0 ]; then
+			echo "Done"
+		fi
 		;;
 esac 
